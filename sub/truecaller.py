0d72d15a0ef81b1983d4c2e674c357a800481def
Katroju Rudranag <rudranag.katroju@gmail.com>
2021-09-10 13:59:22 +0530

    it will return carrier,email,city now

--- sub/truecaller.py
+++ sub/truecaller.py
@@ -1,5 +1,5 @@
 import  requests
-
+length=0
 
 #headers required and bearer is authorization token
 
@@ -11,19 +11,33 @@ head={
  } 
   
 def main(num):
+    global length
+    info=[]
     urlapi='https://search5-noneu.truecaller.com/v2/search?countryCode=IN&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&adId=1af646b1-8cdf-4e8a-b9d4-349f546890c5&encoding=json&q='
-    try:
-        url=urlapi+str(num)
-        req=requests.get(url,headers=head)
-        data=req.json()
+    url=urlapi+str(num)
+    req=requests.get(url,headers=head)
+    data=req.json()
+    try:        
         name=data['data'][0]['name']
-        return name
-        
-    except Exception as e:
-        pass
-				
-		
-			
-												
+        info.append(name)     
+    except : pass              
+    try:                
+        carrier=data['data'][0]['phones'][0]['carrier']  
+        info.append(carrier)              
+    except : pass
+    try:                
+        email=data['data'][0]['internetAddresses'][0]['id']  
+        info.append(email)              
+    except : pass 
+    try:                
+        address=data['data'][0]['addresses'][0]['city']  
+        info.append(address)              
+    except : pass
+         
+    length=len(info)
+    
+    return info
 
 
+
+print(main("8977570270"))
