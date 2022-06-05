cc520d54e4f80c0c818e5f706d40fbe422214011
Osama been lagging <rudranag.katroju@gmail.com>
2022-06-05 19:42:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ Flag=0
 # flag is used to know where the input number should go 
 
 #add bot api from environment variables
-api=os.environ.get('api_key')
+#api=os.environ.get('api_key')
 
 # add api key without environment variables
-#api=YOUR_API_KEY
+api=YOUR_API_KEY
 
 # create a log file
 logging.basicConfig(filename='bot.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
