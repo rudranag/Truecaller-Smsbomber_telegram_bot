cc589ce4b13f462a6b15e45b21fc587f0d3adfb0
Sbulls68 <111579867+Sbulls68@users.noreply.github.com>
2022-08-19 19:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Flag=0
 #api=os.environ.get('api_key')
 
 # add api key without environment variables
-api=YOUR_API_KEY
+api=YOUR_API_
 
 # create a log file
 logging.basicConfig(filename='bot.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
